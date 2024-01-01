f205fccf59dfe1efe6bde8f8b153443e194e4105
aziham <hamza.aziane@pm.me>
2024-01-01 20:11:26 +0100

    Implement 'subtract' function

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const btnsContainer = document.querySelector(".controls");
 // Addition function
 const add = (num1, num2) => num1 + num2;
 
-
+// Subtraction function
+const subtract = (num1, num2) => num1 - num2;
 
 
 
