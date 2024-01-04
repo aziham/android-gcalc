7a46742c2eed262726a2853fb9d97464df2aa0d9
aziham <hamza.aziane@pm.me>
2024-01-04 23:54:34 +0100

    Implement 'tokenize' function

--- app.js
+++ app.js
@@ -46,7 +46,35 @@ const operate = (operator, num1, num2) => {
 	return result;
 }
 
+/* Tokenize function: Takes a mathematical expression as a string and tokenizes it, breaking it down into individual elements such as numbers, operators, and parentheses. The resulting array represents the expression in infix notation */
+const tokenize = expression => {
+	const infixNotation = [];
+	let currentToken = "";
 
+	for (const char of expression) {
+		// Check if the character is a digit or decimal point
+		if (/\d|\./.test(char)) {
+			currentToken += char;
+		}
+		// Check if the character is an operator or parenthesis
+		else if (/[\+\-×÷%()]/.test(char)) {
+			// If there's a current token, push it to infixNotation[]
+			if (currentToken !== "") {
+				infixNotation.push(parseFloat(currentToken));
+				// Preparing the currentToken for the next iteration
+				currentToken = "";
+			}
+			// Push the operator or parenthesis after the currentToken is pushed
+			infixNotation.push(char);
+		}
+	}
+
+	// Push the last token in the input expression if it's not an empty string
+	if (currentToken !== "")
+		infixNotation.push(parseFloat(currentToken));
+
+	return infixNotation;
+}
 
 
 
